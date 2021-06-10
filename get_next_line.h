68d981a09be956afeb4e5cdad0a0ad7376f6b3d9
Sebastien Patez <spatez@shi-r10-p7.s19.be>
2021-06-10 02:45:51 +0200

    get next line debut

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spatez <spatez@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/10 00:53:18 by spatez            #+#    #+#             */
+/*   Updated: 2021/06/10 01:06:22 by spatez           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+int get_next_line(int fd, char **line);
+
+#endif
