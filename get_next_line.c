271d0b8a974522c55d83de5e430527428e37609a
Sebastien Patez <spatez@shi-r10-p7.s19.be>
2021-06-14 10:43:46 +0200

    probleme de stream qui se reset pas

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,119 @@
 /*   By: spatez <spatez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 00:53:22 by spatez            #+#    #+#             */
-/*   Updated: 2021/06/14 07:48:52 by spatez           ###   ########.fr       */
+/*   Updated: 2021/06/14 10:25:05 by spatez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	n;
+
+	n = 0;
+	while (s[n])
+		n++;
+	return (n);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	new = malloc(sizeof(*new) * (ft_strlen(s) + 1));
+	if (!new)
+		return (0);
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (!dst || !src)
+		return (0);
+	if (size == 0)
+		return (ft_strlen(src));
+	i = -1;
+	while (src[++i] && i < size - 1)
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
 ssize_t	ft_line_return_check(char *buffer)
 {
 	// returns the index for the first occurence of \n
-	// -1 if no \n was found and EOF was reached
-	// buffer_size + 1 if \n not found
+	// -1 if \n not found
 	int i;
 
 	i = -1;
+	buffer[BUFFER_SIZE] = '\0';
 	while (buffer[++i])
+    {
 		if (buffer[i] == '\n')
-			return (i);
+		{
+            if (i < BUFFER_SIZE - 1)
+                buffer[i] = '\0';
+            return (i);
+        }
+    }
+	// printf("%s\n", buffer);
 	return (-1);
 }
 
 
 
-ssize_t ft_read(int fd, char *buffer)
+char *ft_read(int fd, char *buffer)
 {
     static int malloced;
     int i;
+    int eof;
+    char *dst;
 
     if (malloced)
         free(buffer);
-    buffer = malloc(BUFFER_SIZE);
+    buffer = malloc((BUFFER_SIZE) + 1);
     if (!buffer)
-        return (-1);
+        return (NULL);
     malloced = 1;
-    read(fd, buffer, BUFFER_SIZE);
+    if (read(fd, buffer, BUFFER_SIZE) == 0)
+        eof = 1;
     i = ft_line_return_check(buffer);
-
+    if (i == -1)
+        return (ft_read(fd, buffer));
+    
+    dst = ft_strdup(buffer);
+    return (dst);
 }
 
 int get_next_line(int fd, char **line)
 {
     char *buffer;
-
-    check errors
-
-    ft_read(fd, buffer)
+	char *test;
+    // check errors
+    buffer = NULL;
+    printf("%s",ft_strdup(ft_read(fd, buffer)));
+    test = ft_strdup(ft_read(fd, buffer));
+	printf("%s\n", test);
+    *line = malloc(ft_strlen(test) + 1);
+    if (!*line)
+        return (-1);
+    ft_strlcpy(*line, test, 10000);
+    return(1);
 }
 
 
@@ -67,5 +135,4 @@ int get_next_line(int fd, char **line)
 // 	return (1);
 	
 // }
-
 
